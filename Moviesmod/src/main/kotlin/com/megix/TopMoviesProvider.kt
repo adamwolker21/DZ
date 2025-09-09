018a5e7ddc8292b755ad951938f202f521a3db76
adamwolker21 <83883573+adamwolker21@users.noreply.github.com>
2025-09-09 23:10:46 +0100

    Build 1de7364cd913c82ba97ef1d0d8c5469a2cb36cd8

---
+++ Moviesmod/src/main/kotlin/com/megix/TopMoviesProvider.kt
@@ -0,0 +1,66 @@
+package com.megix
+
+import com.lagradost.cloudstream3.*
+import com.lagradost.cloudstream3.utils.*
+import org.jsoup.nodes.Element
+import org.jsoup.select.Elements
+import kotlinx.coroutines.runBlocking
+import org.json.JSONObject
+
+class TopmoviesProvider : MoviesmodProvider() { // all providers must be an instance of MainAPI
+    override var mainUrl = "https://topmovies.rodeo"
+    override var name = "TopMovies"
+    override val hasMainPage = true
+    override var lang = "hi"
+    override val hasDownloadSupport = true
+    override val supportedTypes = setOf(
+        TvType.Movie,
+        TvType.TvSeries
+    )
+
+    init {
+        runBlocking {
+            basemainUrl?.let {
+                mainUrl = it
+            }
+        }
+    }
+
+    companion object {
+        val basemainUrl: String? by lazy {
+            runBlocking {
+                try {
+                    val response = app.get("https://raw.githubusercontent.com/SaurabhKaperwan/Utils/refs/heads/main/urls.json")
+                    val json = response.text
+                    val jsonObject = JSONObject(json)
+                    jsonObject.optString("topmovies")
+                } catch (e: Exception) {
+                    null
+                }
+            }
+        }
+    }
+
+    override val mainPage = mainPageOf(
+        "/page/" to "Home",
+        "/web-series/page/" to "Latest Web Series",
+        "/movies/hindi-movies/page/" to "Latest Hindi Movies",
+    )
+
+    override suspend fun search(query: String): List<SearchResponse> {
+        val searchResponse = mutableListOf<SearchResponse>()
+
+        for (i in 1..7) {
+            val document = app.get("$mainUrl/search/$query/page/$i").document
+
+            val results = document.select("div.post-cards > article").mapNotNull { it.toSearchResult() }
+
+            if (results.isEmpty()) {
+                break
+            }
+            searchResponse.addAll(results)
+        }
+
+        return searchResponse
+    }
+}
