9306b88493d01225e1c3722afbbb5d1ad23cc606
adamwolker21 <83883573+adamwolker21@users.noreply.github.com>
2025-09-10 00:14:51 +0100

    Create build.gradle.kts

---
+++ FaselHDProvider/build.gradle.kts
@@ -0,0 +1,13 @@
+version = 3
+
+cloudstream {
+    authors = listOf( "ImDZ" )
+
+	language = "ar"
+	
+    status = 1
+
+    tvTypes = listOf( "TvSeries" , "Movie" , "Anime" , "AsianDrama" )
+
+    iconUrl = "https://www.google.com/s2/favicons?domain=faselhd.io&sz=%size%"
+}
