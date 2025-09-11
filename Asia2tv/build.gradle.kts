455d016936e19c3f10628d97ff044119ce8e9618
adamwolker21 <83883573+adamwolker21@users.noreply.github.com>
2025-09-12 00:01:18 +0100

    Create build.gradle.kts

---
+++ Asia2tv/build.gradle.kts
@@ -0,0 +1,16 @@
+
+version = 1
+
+cloudstream {
+    language = "ar"
+    description = "إضافة لمشاهدة الأفلام والمسلسلات من موقع فاصل إعلاني"
+    authors = listOf("adamwolker21")
+
+    status = 1 
+    tvTypes = listOf(
+        "Movie",
+        "TvSeries",
+    )
+
+    iconUrl = "https://i.imgur.com/example.png"
+}
