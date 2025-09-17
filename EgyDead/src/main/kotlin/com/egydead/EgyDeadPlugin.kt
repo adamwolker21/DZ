a4522187f51cca0fde9aa907dd31d4f0d8322447
adamwolker21 <83883573+adamwolker21@users.noreply.github.com>
2025-09-17 02:33:12 +0100

    Update EgyDeadPlugin.kt

--- EgyDead/src/main/kotlin/com/egydead/EgyDeadPlugin.kt
+++ EgyDead/src/main/kotlin/com/egydead/EgyDeadPlugin.kt
@@ -1,15 +1,12 @@
 package com.egydead
 
-import android.content.Context
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
+import android.content.Context
 
-class EgyDeadPlugin : CloudstreamPlugin() {
-    override fun load(context: Context): Plugin {
-        return Plugin(
-            name = "EgyDead",
-            version = 1,
-            provider = EgyDeadProvider()
-        )
+@CloudstreamPlugin
+class EgyDeadPlugin: Plugin() {
+    override fun load(context: Context) {
+        registerMainAPI(EgyDeadProvider())
     }
 }
