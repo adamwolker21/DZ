74211e47dc7f9963cfa431968ab6e323e94c121e
adamwolker21 <83883573+adamwolker21@users.noreply.github.com>
2025-10-06 00:59:39 +0100

    Create EgyDeadUtils.kt

---
+++ EgyDead/src/main/kotlin/com/egydead/EgyDeadUtils.kt
@@ -0,0 +1,14 @@
+package com.egydead
+
+import com.lagradost.cloudstream3.app
+import org.jsoup.nodes.Document
+
+object EgyDeadUtils {
+    // This function will be the core of our solution.
+    // It will take a URL, perform the POST request, and return the full page document.
+    suspend fun getPageWithEpisodes(url: String): Document? {
+        // To be implemented in the next version.
+        // It will contain the logic for GET -> POST with cookies and headers.
+        return null
+    }
+}
