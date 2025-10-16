dde2d9c30c4710affb62b648670980c26e78a27b
adamwolker21 <83883573+adamwolker21@users.noreply.github.com>
2025-10-16 01:08:07 +0100

    Create build.gradle.kts

---
+++ Asiatv.one/build.gradle.kts
@@ -0,0 +1,15 @@
+version = 1
+
+cloudstream {
+    language = "ar"
+    description = "EgyDead"
+    authors = listOf("adamwolker21")
+
+    status = 1 
+    tvTypes = listOf(
+        "Movie",
+        "TvSeries",
+    )
+
+    iconUrl = "https://raw.githubusercontent.com/adamwolker21/DZ/refs/heads/master/EgyDead/Egydead.png"
+}
