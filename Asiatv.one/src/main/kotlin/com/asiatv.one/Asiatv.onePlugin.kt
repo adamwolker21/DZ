4defd3accdbe868e33d61f7a6334e10d6febbbae
adamwolker21 <83883573+adamwolker21@users.noreply.github.com>
2025-10-16 01:41:24 +0100

    Update Asiatv.onePlugin.kt

--- Asiatv.one/src/main/kotlin/com/asiatv.one/Asiatv.onePlugin.kt
+++ Asiatv.one/src/main/kotlin/com/asiatv.one/Asiatv.onePlugin.kt
@@ -1,1 +1,14 @@
+package com.asiatv.one
 
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+import android.content.Context
+
+@CloudstreamPlugin
+class AsiatvonePlugin : Plugin() {
+    override fun load(context: Context) {
+        // All providers should be added in this manner.
+        // Please don't edit the provider name.
+        registerMainAPI(AsiatvoneProvider())
+    }
+}
