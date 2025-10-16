d32469ce135e8ecbdd64b95fa340eb2a85097f86
adamwolker21 <83883573+adamwolker21@users.noreply.github.com>
2025-10-16 22:46:13 +0100

    Update Asiatv.oneExtractors.kt

--- Asiatv.one/src/main/kotlin/com/asiatv.one/Asiatv.oneExtractors.kt
+++ Asiatv.one/src/main/kotlin/com/asiatv.one/Asiatv.oneExtractors.kt
@@ -1,80 +1,79 @@
 package com.asiatv.one
 
 import com.lagradost.cloudstream3.app
-import com.lagradost.cloudstream3.utils.ExtractorApi
-import com.lagradost.cloudstream3.utils.ExtractorLink
-import com.lagradost.cloudstream3.utils.getAndUnpack
+import com.lagradost.cloudstream3.utils.*
 import com.lagradost.cloudstream3.SubtitleFile
-import com.lagradost.cloudstream3.utils.M3u8Helper
 import com.lagradost.cloudstream3.Qualities
-import com.lagradost.cloudstream3.newExtractorLink
-import com.lagradost.cloudstream3.ExtractorLinkType
+import com.lagradost.cloudstream3.extractors.helper.AesHelper
+import javax.crypto.spec.SecretKeySpec
+import com.lagradost.cloudstream3.utils.M3u8Helper
+import com.lagradost.cloudstream3.extractors.MultiQuality
+import com.lagradost.cloudstream3.base64Decode
+import org.jsoup.Jsoup
+import com.lagradost.cloudstream3.MainAPI
+import com.lagradost.cloudstream3.getAndUnpack
+import com.lagradost.cloudstream3.amap
+import com.lagradost.cloudstream3.apmap
+import com.lagradost.cloudstream3.M3u8Load
+import com.lagradost.cloudstream3.unM3u8
+import com.lagradost.cloudstream3.scripting.safeUnpack
+import com.lagradost.cloudstream3.extractors.Vidstream
 
-// Base class for custom extractors
-abstract class AsiaTvExtractor : ExtractorApi() {
-    override val name = "AsiaTvCustom"
-    override var mainUrl = "" // Corrected to var
+// We use QuickExtractor as it's the modern and stable way
+open class AsiaTvPlayer : QuickExtractor() {
+    override var name = "AsiaTvPlayer"
+    override var mainUrl = "https://www.asiatvplayer.com"
     override val requiresReferer = true
 
-    // getUrl will be implemented by specific extractor classes
-    override suspend fun getUrl(
-        url: String,
-        referer: String?,
-        subtitleCallback: (SubtitleFile) -> Unit,
-        callback: (ExtractorLink) -> Unit,
-    ) {}
-}
-
-// Extractor for AsiaTvPlayer
-class AsiaTvPlayer : AsiaTvExtractor() {
-    override val name = "AsiaTvPlayer"
-    override var mainUrl = "asiatvplayer.com"
-
-    override suspend fun getUrl(
-        url: String,
-        referer: String?,
-        subtitleCallback: (SubtitleFile) -> Unit,
-        callback: (ExtractorLink) -> Unit
-    ) {
+    override suspend fun getUrl(url: String, referer: String?): List<ExtractorLink>? {
         val embedHeaders = mapOf("Referer" to (referer ?: "https://asiawiki.me/"))
         val document = app.get(url, headers = embedHeaders).document
         
-        val script = document.selectFirst("script:containsData(eval)")?.data() ?: return
+        val script = document.selectFirst("script:containsData(eval)")?.data() ?: return null
         val unpackedScript = getAndUnpack(script)
         
+        // This will be our list of links to return
+        val links = mutableListOf<ExtractorLink>()
+
+        // Extract direct MP4 links
         val mp4Regex = Regex("""file:"([^"]+\.mp4)"\s*,\s*label:"([^"]+)"""")
         mp4Regex.findAll(unpackedScript).forEach { match ->
             val fileUrl = match.groupValues[1]
             val qualityLabel = match.groupValues[2]
-            callback(
-                newExtractorLink(
+            links.add(
+                ExtractorLink(
                     source = this.name,
                     name = "${this.name} MP4",
                     url = fileUrl,
-                    type = ExtractorLinkType.FILE
-                ) {
-                    this.referer = url
-                    this.quality = when {
+                    referer = url,
+                    quality = when {
                         qualityLabel.contains("720") -> Qualities.P720.value
                         qualityLabel.contains("360") -> Qualities.P360.value
                         else -> Qualities.Unknown.value
-                    }
-                }
+                    },
+                    isM3u8 = false
+                )
             )
         }
 
+        // Extract m3u8 link and generate qualities from it
         val m3u8Url = Regex("""file:"(.*?(?:\.m3u8))"""").find(unpackedScript)?.groupValues?.get(1)
         if (m3u8Url != null) {
             val m3u8Headers = mapOf(
-                "Origin" to "https://www.asiatvplayer.com",
-                "Referer" to "https://www.asiatvplayer.com/"
+                "Origin" to this.mainUrl,
+                "Referer" to this.mainUrl + "/"
             )
+            // M3u8Helper will return a list of ExtractorLinks
             M3u8Helper.generateM3u8(
-                this.name,
-                m3u8Url,
-                "https://www.asiatvplayer.com/",
+                name = this.name,
+                streamUrl = m3u8Url,
+                referer = this.mainUrl + "/",
                 headers = m3u8Headers
-            ).forEach(callback)
+            ).forEach { link ->
+                links.add(link)
+            }
         }
+        
+        return links
     }
                             }
