5b2fbb0093983e9953a6e04c1d3dff23133b5b6b
adamwolker21 <83883573+adamwolker21@users.noreply.github.com>
2026-08-06 00:10:22 +0100

    Create build.gradle.kts

---
+++ Fivetv/build.gradle.kts
@@ -0,0 +1,16 @@
+
+version = 1
+
+cloudstream {
+    language = "ar"
+    description = "موقع Fivetv فايف تي في"
+    authors = listOf("adamwolker21")
+
+    status = 1 
+    tvTypes = listOf(
+        "Movie",
+        "TvSeries",
+    )
+
+    iconUrl = "https://5tv.lol/wp-content/uploads/2024/04/cropped-cropped-cropped-cropped-wwe-300x108-1-2-2-4-1-2.png"
+}
