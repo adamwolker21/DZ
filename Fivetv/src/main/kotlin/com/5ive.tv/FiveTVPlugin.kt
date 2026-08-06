14d2d726838d5e7f89a2da417136013be9a5cef0
adamwolker21 <83883573+adamwolker21@users.noreply.github.com>
2026-08-06 01:24:55 +0100

    Update FiveTVPlugin.kt

--- Fivetv/src/main/kotlin/com/5ive.tv/FiveTVPlugin.kt
+++ Fivetv/src/main/kotlin/com/5ive.tv/FiveTVPlugin.kt
@@ -1,1 +1,13 @@
+package com.5ive.tv
 
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+import android.content.Context
+
+@CloudstreamPlugin
+class FiveTVPlugin: Plugin() {
+    override fun load(context: Context) {
+        // تسجيل المزود (Provider) عند تشغيل الإضافة
+        registerMainAPI(FiveTVProvider())
+    }
+}
