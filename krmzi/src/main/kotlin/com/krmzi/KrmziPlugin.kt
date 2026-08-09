70c69849cde51c413447326e397da4b806c0bd7b
adamwolker21 <83883573+adamwolker21@users.noreply.github.com>
2026-08-09 04:44:13 +0100

    Update KrmziPlugin.kt

--- krmzi/src/main/kotlin/com/krmzi/KrmziPlugin.kt
+++ krmzi/src/main/kotlin/com/krmzi/KrmziPlugin.kt
@@ -1,1 +1,16 @@
+package com.krmzi
 
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+import android.content.Context
+
+@CloudstreamPlugin
+class KrmziPlugin : Plugin() {
+    override fun load(context: Context) {
+        // تسجيل المزود الأساسي
+        registerMainAPI(KrmziProvider())
+        
+        // تسجيل المستخرجات الخاصة (في حال احتجنا إضافة مستخرج لسيرفر خاص مثل Arab HD)
+        registerExtractorAPI(KrmziExtractor())
+    }
+}
