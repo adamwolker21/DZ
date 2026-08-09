ee2368c9a2d67ee8355e3d921c2eb90f3c8efe9b
adamwolker21 <83883573+adamwolker21@users.noreply.github.com>
2026-08-09 06:33:38 +0100

    Update KrmziExtractors.kt

--- Krmzi/src/main/kotlin/com/krmzi/KrmziExtractors.kt
+++ Krmzi/src/main/kotlin/com/krmzi/KrmziExtractors.kt
@@ -1,67 +1,67 @@
 package com.krmzi
 
-import com.lagradost.cloudstream3.SubtitleFile
 import com.lagradost.cloudstream3.app
 import com.lagradost.cloudstream3.utils.ExtractorApi
 import com.lagradost.cloudstream3.utils.ExtractorLink
+import com.lagradost.cloudstream3.utils.ExtractorLinkType
+import com.lagradost.cloudstream3.utils.JsUnpacker
 import com.lagradost.cloudstream3.utils.Qualities
-import com.lagradost.cloudstream3.utils.Unpacker
+import com.lagradost.cloudstream3.utils.newExtractorLink
 
 // مستخرج سيرفر Arab HD
 open class ArabHdExtractor(
-    override val name: String = "Arab HD",
-    override val mainUrl: String = "https://arabhd.onl"
+    override var name: String = "Arab HD",
+    override var mainUrl: String = "arabhd.onl"
 ) : ExtractorApi() {
     override val requiresReferer = true
 
-    override suspend fun getUrl(
-        url: String,
-        referer: String?,
-        subtitleCallback: (SubtitleFile) -> Unit,
-        callback: (ExtractorLink) -> Unit
-    ) {
+    override suspend fun getUrl(url: String, referer: String?): List<ExtractorLink>? {
+        val extractedLinks = mutableListOf<ExtractorLink>()
         try {
-            val response = app.get(url, referer = mainUrl)
+            val response = app.get(url, referer = "https://$mainUrl")
             val doc = response.document
             
             // البحث عن السكريبت المشفر بصيغة eval
             val script = doc.select("script").find { it.data().contains("eval(function") }?.data() ?: ""
             
-            // استخدام أداة فك التشفير الخاصة بـ Cloudstream
+            // فك التشفير باستخدام الأداة الحديثة JsUnpacker
             val unpacked = if (script.isNotEmpty()) {
-                Unpacker.unpack(script)
+                JsUnpacker(script).unpack() ?: response.text
             } else {
                 response.text
             }
 
-            // استخراج رابط الـ m3u8 باستخدام Regex (التعبيرات النمطية)
+            // استخراج رابط الـ m3u8
             val m3u8Regex = Regex("""file\s*:\s*["']([^"']+\.m3u8[^"']*)["']""")
             val match = m3u8Regex.find(unpacked)
             
             if (match != null) {
                 val m3u8Url = match.groupValues[1]
                 
-                // إرسال الرابط للتطبيق مع الـ Headers الضرورية لتخطي الحماية
-                callback.invoke(
-                    ExtractorLink(
+                // إنشاء الرابط وإرجاعه للتطبيق بالنظام الجديد
+                extractedLinks.add(
+                    newExtractorLink(
                         source = this.name,
                         name = this.name,
                         url = m3u8Url,
-                        referer = mainUrl,
-                        quality = Qualities.Unknown.value,
-                        isM3u8 = true,
-                        headers = mapOf(
-                            "Origin" to mainUrl,
-                            "Referer" to "$mainUrl/"
+                        type = ExtractorLinkType.M3U8
+                    ) {
+                        this.referer = "https://$mainUrl/"
+                        this.quality = Qualities.Unknown.value
+                        this.headers = mapOf(
+                            "Origin" to "https://$mainUrl",
+                            "Referer" to "https://$mainUrl/"
                         )
-                    )
+                    }
                 )
             }
         } catch (e: Exception) {
             e.printStackTrace()
         }
+        
+        return if (extractedLinks.isNotEmpty()) extractedLinks else null
     }
 }
 
-// مستخرج سيرفر Red HD (يعمل بنفس آلية Arab HD تقريباً ولكن بنطاق مختلف)
-class RedHdExtractor : ArabHdExtractor("Red HD", "https://redhd.onl")
+// مستخرج سيرفر Red HD
+class RedHdExtractor : ArabHdExtractor("Red HD", "redhd.onl")
