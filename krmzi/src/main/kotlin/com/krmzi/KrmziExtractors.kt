65f61ddeebe8c8eaee2a3285e5468a7583606aba
adamwolker21 <83883573+adamwolker21@users.noreply.github.com>
2026-08-09 04:44:43 +0100

    Update KrmziExtractors.kt

--- krmzi/src/main/kotlin/com/krmzi/KrmziExtractors.kt
+++ krmzi/src/main/kotlin/com/krmzi/KrmziExtractors.kt
@@ -1,1 +1,23 @@
+package com.krmzi
 
+import com.lagradost.cloudstream3.utils.ExtractorApi
+import com.lagradost.cloudstream3.utils.ExtractorLink
+import com.lagradost.cloudstream3.utils.INFERNO_URL
+import com.lagradost.cloudstream3.utils.Qualities
+
+// مستخرج إضافي لروابط أو سيرفرات غير مدعومة افتراضياً في التطبيق
+// تم تجهيز هذا الملف ليكون جاهزاً في حال أردت مستقبلاً سحب سيرفرات خاصة مثل (Arab HD) و (Red HD)
+class KrmziExtractor : ExtractorApi() {
+    override val name = "Krmzi VIP"
+    override val mainUrl = "https://krmzi.org" // نطاق المستخرج
+    override val requiresReferer = false
+
+    override suspend fun getUrl(
+        url: String,
+        referer: String?,
+        subtitleCallback: (SubtitleFile) -> Unit,
+        callback: (ExtractorLink) -> Unit
+    ) {
+        // يمكننا إضافة أكواد الاستخراج الخاصة هنا لاحقاً
+    }
+}
