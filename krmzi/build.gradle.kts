e7a691f6ab0dcd447920d0c7c29bd20119074712
adamwolker21 <83883573+adamwolker21@users.noreply.github.com>
2026-08-09 04:09:42 +0100

    Create build.gradle.kts

---
+++ krmzi/build.gradle.kts
@@ -0,0 +1,15 @@
+version = 1
+
+cloudstream {
+    language = "ar"
+    description = "قرمزي – المنصة رقم #1 لمشاهدة المسلسلات التركية"
+    authors = listOf("adamwolker21")
+
+    status = 1 
+    tvTypes = listOf(
+        "Movie",
+        "TvSeries",
+    )
+
+    iconUrl = "https://krmzi.org/wp-content/uploads/2026/02/cropped-faviconkrmzi.png"
+}
